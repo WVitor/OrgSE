aea238cff386e27833791dbb3c84cec7fbe6d904
WVitor <72871331+WVitor@users.noreply.github.com>
2023-01-11 08:53:28 -0300

    ajustes de sintaxe

--- src/Controller.js
+++ src/Controller.js
@@ -1,126 +1,123 @@
-const fs = require('fs')
 const axios = require('axios')
 const API_URL = process.env.API_URL
 module.exports = class Controller{
-    static async main(req, res, rotas){
-        try {
-            const planos = await axios.get(`${API_URL}?PRIMEIRO=true`)
-            .then(function (response) {
-              // handle success
-              return response.data;
-            })
-           return res.render('pages/home', {planos: planos})
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    static async mainPages(req, res){
-        try {
-            const topicosFilhos = []
-            //topico pai
-            let topicoPai = await axios.get(`${API_URL}?URL=${req.path.slice(1)}`)
-            .then(async function (response) {
-              // handle success
-                return response.data[0];
-                //topicos filhos
-               
-            })
-            if(topicoPai){
-               await axios.get(`${API_URL}?TOPICOPAIID=${topicoPai.id}&_sort=id&_order=asc`)
-                .then(function (res) {
-                  // handle success
-                  return res.data.forEach(valor => {
-                    topicosFilhos.push(valor)
-                  });;
-                })
-                return res.render('pages/templatePage', {topicoPai: topicoPai, topicosFilhos: topicosFilhos})
-            }else{
-              return res.redirect('/')
-            }
-
-            
-            
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    static async criarPlanoDeEstudos(req, res){
-        try {
-           let topicoExists = false
-
-           return res.render('pages/criarPlanoDeEstudos', {topicoExists})
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    static async criarPlanoDeEstudosPost(req, res){
-        try {
-            const TOPICO = req.body.topico
-            let topicoExists = false
-            await axios.get(`${API_URL}?TOPICO=${TOPICO}`)
-            .then(function (response) {
-              // handle success
-              if(response.data.length > 0 && response.data[0].TOPICO == TOPICO){
-                topicoExists = true
-              }
-              return
-            })
-
-            if(!topicoExists){
-                await axios.post(API_URL, {
-                    TOPICO,
-                    TOPICOPAIID: "0",
-                    PRIMEIRO: true,
-                    URL: TOPICO.toLowerCase().split(" ").join("-"),
-                    STATUS: false
-                })
-                return res.redirect('/')
-            }
-            else{
-                return res.render('pages/criarPlanoDeEstudos', {topicoExists})
-            }
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    static async excluirPlanoDeEstudos(req, res){
-        try {
-            const TOPICOID = req.body.id
-            const topicosFilhos = [] 
-            await axios.get(`${API_URL}?TOPICOPAIID=${TOPICOID}`)
-            .then(async function (response) {
-              // handle success         
-                return response.data.map((valor)=>{
-                    topicosFilhos.push(valor)
-                })             
-            })
-            if(topicosFilhos.length > 0){
-              for(var i = 0; i < topicosFilhos.length; i++){
-                await axios.delete(`${API_URL}/${topicosFilhos[i].id}`)
-              }
-            }
-            await axios.delete(`${API_URL}/${TOPICOID}`).then(()=>{
-              return  res.redirect('/')
-            })
-        } catch (error) {
-            console.log(error)
-        }
-    }
-
-    static async criarTopicoFilho(req, res){
+  static async main(req, res, rotas){
       try {
-         let topicoExists = false
-          const topicos = await axios.get(`${API_URL}?PRIMEIRO=true`).then((response)=>{
-            return response.data
+          const planos = await axios.get(`${API_URL}?PRIMEIRO=true`)
+          .then(function (response) {
+            return response.data;
           })
-         return res.render('pages/criarTopicoFilho', {topicoExists, topicos})
+          return res.render('pages/home', {planos: planos})
       } catch (error) {
           console.log(error)
       }
+  }
+
+  static async mainPages(req, res){
+      try {
+          const topicosFilhos = []
+          
+          let topicoPai = await axios.get(`${API_URL}?URL=${req.path.slice(1)}`)
+          .then(async function (response) {
+              return response.data[0];
+          })
+
+          if(topicoPai){
+              await axios.get(`${API_URL}?TOPICOPAIID=${topicoPai.id}&_sort=id&_order=asc`)
+              .then(function (res) {
+                return res.data.forEach(valor => {
+                  topicosFilhos.push(valor)
+                });;
+              })
+              return res.render('pages/templatePage', {topicoPai: topicoPai, topicosFilhos: topicosFilhos})
+          }else{
+            return res.redirect('/')
+          }   
+      } catch (error) {
+          console.log(error)
+      }
+  }
+
+  static async criarPlanoDeEstudos(req, res){
+      try {
+          let topicoExists = false
+          return res.render('pages/criarPlanoDeEstudos', {topicoExists})
+      } catch (error) {
+          console.log(error)
+      }
+  }
+
+  static async criarPlanoDeEstudosPost(req, res){
+      try {
+          const TOPICO = req.body.topico
+          let topicoExists = false
+
+          await axios.get(`${API_URL}?TOPICO=${TOPICO}`)
+          .then(function (response){
+            if(response.data.length > 0 && response.data[0].TOPICO == TOPICO){
+              topicoExists = true
+            }
+            return
+          })
+
+          if(!topicoExists){
+              await axios.post(API_URL, {
+                  TOPICO,
+                  TOPICOPAIID: "0",
+                  PRIMEIRO: true,
+                  URL: TOPICO.toLowerCase().split(" ").join("-"),
+                  STATUS: false
+              })
+              return res.redirect('/')
+          }
+          else{
+              return res.render('pages/criarPlanoDeEstudos', {topicoExists})
+          }
+      } catch (error) {
+          console.log(error)
+      }
+  }
+
+  static async excluirPlanoDeEstudos(req, res){
+      try {
+          const TOPICOID = req.body.id
+          const topicosFilhos = [] 
+
+          await axios.get(`${API_URL}?TOPICOPAIID=${TOPICOID}`)
+          .then(async function (response) {
+            // handle success         
+              return response.data.map((valor)=>{
+                  topicosFilhos.push(valor)
+              })             
+          })
+
+          if(topicosFilhos.length > 0){
+            for(var i = 0; i < topicosFilhos.length; i++){
+              await axios.delete(`${API_URL}/${topicosFilhos[i].id}`)
+            }
+          }
+
+          await axios.delete(`${API_URL}/${TOPICOID}`)
+          .then(()=>{
+            return  res.redirect('/')
+          })
+
+      } catch (error) {
+          console.log(error)
+      }
+  }
+
+  static async criarTopicoFilho(req, res){
+    try {
+        let topicoExists = false
+        const topicos = await axios.get(`${API_URL}?PRIMEIRO=true`)
+        .then((response)=>{
+          return response.data
+        })
+        return res.render('pages/criarTopicoFilho', {topicoExists, topicos})
+    } catch (error) {
+        console.log(error)
+    }
   }
 
   static async criarTopicoFilhoPost(req, res){
@@ -128,6 +125,7 @@ module.exports = class Controller{
           const TOPICO = req.body.topico
           const TOPICOPAIID = req.body.topicoPaiId
           let topicoExists = false
+
           await axios.get(`${API_URL}?TOPICO=${TOPICO}`)
           .then(function (response) {
             // handle success
@@ -145,7 +143,8 @@ module.exports = class Controller{
                   URL: "",
                   STATUS: false
               })
-              const TOPICOPAIURL = await axios.get(`${API_URL}/${TOPICOPAIID}`).then((response)=>{
+              const TOPICOPAIURL = await axios.get(`${API_URL}/${TOPICOPAIID}`)
+              .then((response)=>{
                 return response.data.URL
               })
               return res.redirect(`/${TOPICOPAIURL}`)
